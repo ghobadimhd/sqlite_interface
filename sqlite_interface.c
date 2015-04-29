99b7bb61cd5cc0fe64cbe8e950ad755cedb744a2
Ghobadimhd <Ghobadi.mhd@outlook.com>
2015-04-29 19:35:20 +0430

    initialing interface

---
+++ sqlite_interface.c
@@ -0,0 +1,16 @@
+/*
+sqlite_interface	2015-04-29 19:04
+Program name : sqlite interface
+Author : Mohammad Ghobadi
+Email : ghobadi.mhd@outlook.com
+describtion : This is program for implementing simple interface for sqlite3 .
+*/
+//include data structures 
+//data base files and query generators 
+//user interface
+int main(int argc, const char *argv[])
+{
+	//connecting to database 
+	//menu and user interface
+	return 0;
+}
