536d86064fe963e9a623fd194d83bcd4690fee18
Ghobadimhd <Ghobadi.mhd@outlook.com>
2015-04-30 09:11:36 +0430

    query function added

--- db_manager.h
+++ db_manager.h
@@ -13,3 +13,17 @@ int open_db(char *db_name)
 	else 
 		return 1 ; // dataBase (create and ) opened successfully
 }
+/*
+this is function query to database  .
+*/
+int query(char *sql , callbackFunction)
+{
+	int rc ;
+	rc = sqlite3_exec(db, sql, callback, 0, &errorMessage);
+	if( rc != SQLITE_OK )
+	{
+		return 0 ;
+	}
+	 else
+		 return 1 ;
+}
