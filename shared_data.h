6f96e85371376a4b47996ffaf800eaba63d14201
Ghobadimhd <Ghobadi.mhd@outlook.com>
2015-05-06 20:28:42 +0430

    adding some code to ease use of printf

--- shared_data.h
+++ shared_data.h
@@ -1,1 +1,5 @@
-
+#define CLEAR_SCREEN printf("\033[2J");
+// change terminal color 
+#define COLOR_RED printf("\033[31m");
+#define COLOR_GREEN printf("\033[32m");
+#define COLOR_RESET printf("\033[0m");
