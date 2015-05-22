77a87a901e7473e6b1c4cf294041a281c5c465e5
Ghobadimhd <Ghobadi.mhd@outlook.com>
2015-05-22 11:04:07 +0430

    main improved

--- sqlite_interface.c
+++ sqlite_interface.c
@@ -5,20 +5,21 @@ Author : Mohammad Ghobadi
 Email : ghobadi.mhd@outlook.com
 describtion : This is program for implementing simple interface for sqlite3 .
 */
-//include data structures 
-//data base files and query generators 
-//user interface
 #include <stdio.h>
 #include <string.h>
 #include <sqlite3.h>
+#include <stdio.h>
+#include <shared_data.h>
 #include <db_manager.h>
 #include <query_gen.h>
 #include <menu.h>
 int main(int argc, const char *argv[])
 {
 	//connecting to database 
-	connectToDb();
-	mainMenu();
-	//menu and user interface
+	int success = connectToDb();
+	if(success)
+		mainMenu();
+	else 
+		printf("Connecting to database failed .\n");
 	return 0;
 }
