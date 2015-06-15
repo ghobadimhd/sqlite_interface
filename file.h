8e7b8e6ae6de9051d9947367c89a0879e6648e1e
Ghobadimhd <Ghobadi.mhd@outlook.com>
2015-06-15 10:45:05 +0430

    add headers

--- file.h
+++ file.h
@@ -1,3 +1,10 @@
+#ifndef _FILE_H
+#define _FILE_H
+#endif
+
+#ifndef _STDIO_H
+#include <stdio.h>
+#endif
 //
 // this function save a string in end of file 
 int stringWriter(char string[], char addr[])
